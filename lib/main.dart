2de06bcf03892870a7859e5b90ea409508320ad0
Ray Schireman <41241487+rschireman@users.noreply.github.com>
2020-09-07 14:38:53 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_foodybite/screens/main_screen.dart';
 import 'package:flutter_foodybite/util/const.dart';
 import 'package:firebase_admob/firebase_admob.dart';
 import 'package:shake/shake.dart';
-import 'package:geolocator/geolocator.dart';
 
 void main() async {
   runApp(MyApp());
